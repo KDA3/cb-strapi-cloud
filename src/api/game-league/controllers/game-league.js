6040874e6fdf1860c432a56e61c0b9cb21c6f366
Carlos Poon <carlos@kda3.io>
2025-07-26 21:24:06 -0400

    Refactor to add League

---
+++ src/api/game-league/controllers/game-league.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * game-league controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::game-league.game-league');
---
+++ src/api/game-league/routes/game-league.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * game-league router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::game-league.game-league');
