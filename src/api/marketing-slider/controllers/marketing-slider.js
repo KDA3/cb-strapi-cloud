4590ad8eafaff71c58fffe8d43e16ae1b333b0e9
Carlos Poon <carlos@kda3.io>
2025-08-12 17:26:39 -0400

    [UNIFY-477] - Marketing Slider from Strapi

---
+++ src/api/marketing-slider/controllers/marketing-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * marketing-slider controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::marketing-slider.marketing-slider');
---
+++ src/api/marketing-slider/routes/marketing-slider.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * marketing-slider router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::marketing-slider.marketing-slider');
